6dae29d1c95362eec388747142cf3887308c95df
STM <startinmerc@gmail.com>
2020-01-13 14:43:20 +0000

    redux reducers

---
+++ src/store/reducers/currentUser.js
@@ -0,0 +1,21 @@
+import { SET_CURRENT_USER } from "./actionTypes";
+
+const DEFAULT_STATE = {
+	isAuthenticated: false,
+	user: {}
+};
+
+export default (state = DEFAULT_STATE, action) => {
+	switch (action.type) {
+		case SET_CURRENT_USER:
+			return {
+				...state,
+				// !! turns result into boolean,
+				// so false if length is zero
+				isAuthenticated: !!Object.keys(action.user).length,
+				user: action.user
+			};
+		default:
+			return state;
+	}
+};
