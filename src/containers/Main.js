ef3fd27dcfe29b9e9aac7248bfdd788e1c21fbf4
STM <startinmerc@gmail.com>
2020-01-13 16:58:41 +0000

    add Main router

---
+++ src/containers/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Switch, Route, withRouter, Redirect } from "react-router";
+import { connect } from "react-redux";
+import Homepage from "../components/Homepage";
+
+const Main = props => {
+	return (
+		<div className="container">
+			<Switch>
+				<Route exact path="/" render={
+					props => <Homepage {...props}/>
+				} />
+			</Switch>
+		</div>
+	)
+}
+
+function mapStateToProps(state){
+	return {
+		currentUser: state.currentUser
+	}
+}
+
+export default withRouter(connect(mapStateToProps, null)(Main));
