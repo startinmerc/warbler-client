2960572fee1c1bede4aefaf927ef5bac1d250be4
STM <startinmerc@gmail.com>
2020-01-13 15:28:54 +0000

    basic navbar component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from './Navbar';
 
 const store = configureStore();
 
 const App = ()=>(
 	<Provider store={store}>
 		<Router>
+			<div className="onboarding">
+				<Navbar />
+			</div>
 			<div>
 				Hi!
 			</div>
