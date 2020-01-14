e0a9c64a88e5501f4f62392c6778cfc5e1c40ab3
STM <startinmerc@gmail.com>
2020-01-14 13:34:10 +0000

    display errors on Authform, clear on history change

--- src/components/Authform.js
+++ src/components/Authform.js
@@ -21,21 +21,31 @@ class Authform extends Component {
 		e.preventDefault();
 		const authType = this.props.signUp ? "signup" : "signin";
 		this.props.onAuth(authType, this.state).then(()=>{
-			console.log("logged in!");
+			this.props.history.push("/");
 		}).catch(()=>{
-			console.log("error");
+			return;
 		});
 	};
 
 	render(){
 		const { email, username, password, profileImageUrl } = this.state;
-		const { heading, buttonText, signUp } = this.props;
+		const { heading, buttonText, signUp, errors, history, removeError } = this.props;
+		
+		history.listen(()=>{
+			removeError();
+		});
+
 		return(
 			<div>
 				<div className="row justify-content-center text-center">
 					<div className="col-6">
 						<form onSubmit={this.handleSubmit}>
 							<h2>{heading}</h2>
+							{errors.message && 
+								<div className="alert alert-danger">
+									{errors.message}
+								</div>
+							}
 							<label htmlFor="email">Email:</label>
 							<input className="form-control" id="email" name="email"
 							 onChange={this.handleChange} value={email} type="email"
