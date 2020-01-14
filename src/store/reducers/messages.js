d5b8714f83ec660b1d8fbe6bf41f463d774c1c00
STM <startinmerc@gmail.com>
2020-01-14 18:01:42 +0000

    add remove message to reducer

--- src/store/reducers/messages.js
+++ src/store/reducers/messages.js
@@ -4,6 +4,8 @@ const messages = (state=[], action) => {
 	switch(action.type){
 		case LOAD_MESSAGES:
 			return [...action.messages];
+		case REMOVE_MESSAGE:
+			return state.filter(message => message.id !== action.id);
 		default:
 			return state;
 	};
