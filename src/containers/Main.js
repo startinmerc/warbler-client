c0e94c47730152ef3519b46a63345bb8a58a464b
STM <startinmerc@gmail.com>
2020-01-14 13:33:02 +0000

    pass errors from Redux in Main to Authform

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,20 +4,36 @@ import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
 import Authform from "../components/Authform";
 import { authUser } from "../store/actions/auth";
+import { removeError } from "../store/actions/errors";
 
 const Main = props => {
-	const {authUser}=props;
+	const { authUser, errors, removeError, currentUser } = props;
 	return (
 		<div className="container">
 			<Switch>
 				<Route exact path="/" render={
-					props => <Homepage {...props}/>
+					props => <Homepage currentUser={currentUser} {...props}/>
 				} />
 				<Route exact path="/signin" render={
-					props => <Authform onAuth={authUser} buttonText="Sign in" heading="Welcome Back" {...props}/>
+					props => <Authform
+						removeError={removeError}
+						errors={errors}
+						onAuth={authUser}
+						buttonText="Sign in"
+						heading="Welcome Back"
+						{...props}
+					/>
 				} />
 				<Route exact path="/signup" render={
-					props => <Authform onAuth={authUser} signUp buttonText="Sign me up!" heading="Join Warbler" {...props}/>
+					props => <Authform
+						signUp
+						removeError={removeError}
+						errors={errors}
+						onAuth={authUser}
+						buttonText="Sign me up!"
+						heading="Join Warbler"
+						{...props}
+					/>
 				} />
 			</Switch>
 		</div>
@@ -26,8 +42,9 @@ const Main = props => {
 
 function mapStateToProps(state){
 	return {
-		currentUser: state.currentUser
+		currentUser: state.currentUser,
+		errors: state.errors
 	}
 }
 
-export default withRouter(connect(mapStateToProps, { authUser })(Main));
+export default withRouter(connect(mapStateToProps, { authUser, removeError })(Main));
