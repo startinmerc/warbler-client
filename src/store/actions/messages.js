ae2c965c900c6d65a1b0c4039f5c7646fe2178f1
STM <startinmerc@gmail.com>
2020-01-14 18:01:26 +0000

    add remove message action

--- src/store/actions/messages.js
+++ src/store/actions/messages.js
@@ -7,6 +7,11 @@ export const loadMessages = messages => ({
 	messages
 });
 
+export const remove = id => ({
+	type: REMOVE_MESSAGE,
+	id
+});
+
 export const fetchMessages = () => {
 	return dispatch => {
 		return apiCall("get", "/api/messages")
@@ -23,6 +28,10 @@ export const postNewMessage = text => (dispatch, getState) => {
 		.catch(err => dispatch(addError(err.message)));
 }
 
-export const removeMEssage = message => ({
-
-});
+export const removeMessage = (user_id, message_id) => {
+	return dispatch => {
+		return apiCall("delete", `api/users/${user_id}/messages/${message_id}`)
+			.then(() => dispatch(remove(message_id)))
+			.catch(err => dispatch(addError(err.messages)));
+	};
+};
