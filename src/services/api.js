601246b07b7743536ad709fcf33c381872d87e4d
STM <startinmerc@gmail.com>
2020-01-14 14:36:40 +0000

    setTokenHeader in api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+export function setTokenHeader(token){
+	// if token exists
+	if(token){
+		// push token to header
+		axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+	} else {
+		// clear authorization
+		delete axios.defaults.headers.common["Authorization"];
+	}
+}
+
 export function apiCall(method, path, data){
 	return new Promise((resolve, reject) => {
 		return axios[method](path,data)
