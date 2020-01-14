239012ebde5a6ad966be8561651e133f8bc0c12d
STM <startinmerc@gmail.com>
2020-01-14 13:32:05 +0000

    add Errors actions

--- src/store/actions/errors.js
+++ src/store/actions/errors.js
@@ -1,1 +1,10 @@
-errors.js
+import { ADD_ERROR, REMOVE_ERROR } from "../actionTypes";
+
+export const addError = error => ({
+	type: ADD_ERROR,
+	error
+});
+
+export const removeError = ()=>({
+	type: REMOVE_ERROR
+});
