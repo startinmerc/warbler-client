7e8e032864f8427e1c9fc3ce0b32450fdd74bd5a
STM <startinmerc@gmail.com>
2020-01-14 13:34:35 +0000

    conditional Homepage rendering

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Homepage = () => (
-	<div className="home-hero d-flex flex-column justify-content-around align-items-center">
-		<h1>What's Happening?</h1>
-		<div className="container d-flex flex-column align-items-center">
-		<h4>New to Warbler?</h4>
-		<Link to="/signup" className="btn btn-primary">
-			Sign up here!
-		</Link>
-		</div>
-	</div>
-);
+const Homepage = ({ currentUser }) => {
+	if(!currentUser.isAuthenticated){
+		return (
+			<div className="home-hero d-flex flex-column justify-content-around align-items-center">
+				<h1>What's Happening?</h1>
+				<div className="container d-flex flex-column align-items-center">
+					<h4>New to Warbler?</h4>
+					<Link to="/signup" className="btn btn-primary">
+						Sign up here!
+					</Link>
+				</div>
+			</div>	
+		);
+	} else {
+		return (
+			<div>
+				<h1>LOGGEDIN!</h1>
+			</div>
+		);
+	}
+};
 
 export default Homepage;
