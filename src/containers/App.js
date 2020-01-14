7f2250d36534c779e2f59068f8743e793cbb60cf
STM <startinmerc@gmail.com>
2020-01-14 14:38:13 +0000

    check localStorage for existing jwt & add if needed

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,8 +4,22 @@ import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from './Navbar';
 import Main from './Main';
+import { setAuthorizationToken, setCurrentUser } from "../store/actions/user";
+import jwtDecode from "jwt-decode";
 
 const store = configureStore();
+
+if(localStorage.jwtToken){
+	// set Auth token if one is present in ls
+	setAuthorizationToken(localStorage.jwtToken);
+	try {
+		// decode jwtToken, set decoded data as current user
+		store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+	} catch (e) {
+		// clear user if unsucessful
+		store.dispatch(setCurrentUser({}));
+	}
+}
 
 const App = ()=>(
 	<Provider store={store}>
