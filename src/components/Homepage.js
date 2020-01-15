12bc2989c28d0b179361ef610681370e192f0607
STM <startinmerc@gmail.com>
2020-01-15 15:59:23 +0000

    Homepage wording

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,11 +6,11 @@ const Homepage = ({ currentUser }) => {
 	if(!currentUser.isAuthenticated){
 		return (
 			<div className="home-hero d-flex flex-column justify-content-around align-items-center">
-				<h1>What's Happening?</h1>
+				<h1>Welcome to Warbler</h1>
 				<div className="container d-flex flex-column align-items-center">
-					<h4>New to Warbler?</h4>
+					<h4>New around here?</h4>
 					<Link to="/signup" className="btn btn-primary">
-						Sign up here!
+						Sign up!
 					</Link>
 				</div>
 			</div>	
