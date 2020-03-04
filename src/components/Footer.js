5766563adcb2a64ac7be3f74ec8fcc55a4268246
STM <startinmerc@gmail.com>
2020-03-04 18:03:39 +0000

    Footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
 	<footer className="mb-0 py-4 d-flex justify-content-center">
-		<p className="mb-0">
+		<p className="mb-0 text-white">
 			A <a href="http://www.strangeindustries.co.uk">Strange Industries</a> site.
 		</p>
 	</footer>
