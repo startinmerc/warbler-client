4eac83debe3021d85dd84e36e615a199b52b47ed
STM <startinmerc@gmail.com>
2020-03-06 16:30:55 +0000

    UserAside to UserCard

--- src/components/MessageTimeline.js
+++ src/components/MessageTimeline.js
@@ -1,11 +1,11 @@
 import React from "react";
 import MessageList from "../containers/MessageList";
-import UserAside from "./UserAside";
+import UserCard from "./UserCard";
 
 const MessageTimeline = props => {
 	return (
 		<div className="row">
-			<UserAside profileImageUrl={props.profileImageUrl} username={props.username} userID={props.userID}/>
+			<UserCard profileImageUrl={props.profileImageUrl} username={props.username} userID={props.userID}/>
 			<MessageList />
 			<div className="col-2 border border-secondary d-none d-md-block" style={{
 				backgroundColor: "var(--color3)"
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,17 +2,17 @@ import React from "react";
 import Icon from "../images/Icon";
 import { Link } from "react-router-dom";
 
-const UserAside = ({profileImageUrl, username, userID}) => (
+const UserCard = ({profileImageUrl, username, userID}) => (
 	<div className="col-12 col-md-2 mb-3">
-		<div className="card useraside-card">
+		<div className="card user-card">
 		{/* Show profile image, icon as fallback */}
 		{profileImageUrl ? (
 			<img src={profileImageUrl} 
-				alt={username} className="d-none d-md-block card-img-top"
+				alt={username} className="d-none d-md-block card-img-top p-1"
 			/>
 		) : (
-			<div className="d-none d-md-block card-img-top">
-				<Icon size="100%"/>
+			<div className="d-none d-md-block card-img-top p-1">
+				<Icon size="100%" color="var(--vw-blue)"/>
 			</div>
 		)}
 			
@@ -26,4 +26,4 @@ const UserAside = ({profileImageUrl, username, userID}) => (
 	</div>
 );
 
-export default UserAside;
+export default UserCard;
