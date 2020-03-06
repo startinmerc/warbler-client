43b546b2405043f45faebce248c6aa0e25d564da
STM <startinmerc@gmail.com>
2020-03-06 17:14:58 +0000

    MessageItem padding tweak

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -15,7 +15,7 @@ const MessageItem = ({date, profileImageUrl, text, username, removeMessage, isCo
 				<Icon classes="message-item__image py-3 pl-3" size="100px" color="var(--white)"/>
 			)
 		}
-		<div className="ml-2 message-item__content p-3">
+		<div className="ml-2 message-item__content pl-3 py-2">
 			<Link to={`/users/${userId}/messages`}>@{username}</Link>
 			<Moment className="text-muted ml-2" fromNow>
 				{date}
