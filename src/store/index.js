404db58aad324619493f425b7cd059bab7ea8478
STM <startinmerc@gmail.com>
2020-03-06 11:12:32 +0000

    Fix redux_devtools in composer

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,11 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 export function configureStore() {
+	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 	const store = createStore(
 		rootReducer,
-		compose(
-			applyMiddleware(thunk),
-			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		composeEnhancers(
+			applyMiddleware(thunk)
 		)
 	);
 	return store;
