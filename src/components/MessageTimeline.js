5df3e01473982b0969b6266afc6e2c733a65d269
STM <startinmerc@gmail.com>
2020-03-09 17:29:09 +0000

    Adspace tweaking

--- src/components/MessageTimeline.js
+++ src/components/MessageTimeline.js
@@ -7,7 +7,9 @@ const MessageTimeline = props => {
 		<div className="row">
 			<UserCard profileImageUrl={props.profileImageUrl} username={props.username} userID={props.userID}/>
 			<MessageList />
-			<div className="col-2 d-none d-md-block adspace"></div>
+			<div className="col-12 col-md-2 mt-3 mt-md-0">
+				<div className="adspace" />
+			</div>
 		</div>
 	);
 };
