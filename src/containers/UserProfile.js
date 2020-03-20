7b8920fef1f70464838167f145a2c3c1647d8d7a
STM <startinmerc@gmail.com>
2020-03-20 16:35:25 +0000

    Reorganise UserProfile columns

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -5,11 +5,13 @@ const UserProfile = props => {
 	const user = props.location.pathname.split("/")[2];
 	return (
 		<div className="row">
-			<div className="col-12 py-5 text-center">
-				Showing messages from {user}<br/>
-				<small>Yes this is wrong.</small>
+			<div className="col-12 col-md-10">
+				<div className="text-center">
+					Showing messages from {user}<br/>
+					<small>Yes this is wrong.</small>
+				</div>
+				<MessageList fromUser="true" user={user} location={props.location}/>
 			</div>
-			<MessageList  fromUser="true" user={user} location={props.location}/>
 			<div className="col-12 col-md-2">
 				<div className="adspace"/>
 			</div>
