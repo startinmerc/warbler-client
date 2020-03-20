eb667e2e3d600b70f5c3605e3f86202486eedfc1
STM <startinmerc@gmail.com>
2020-03-20 15:35:10 +0000

    Create UserProfile component

---
+++ src/containers/UserProfile.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MessageList from "./MessageList";
+
+const UserProfile = props => {
+	const user = props.location.pathname.split("/")[2];
+	return (
+		<div className="row">
+			<div className="col-12 py-5 text-center">
+				Showing messages from {user}<br/>
+				<small>Yes this is wrong.</small>
+			</div>
+			<MessageList  fromUser="true" user={user} location={props.location}/>
+			<div className="col-12 col-md-2">
+				<div className="adspace sticky-top"/>
+			</div>
+		</div>
+	)
+};
+
+export default UserProfile;
