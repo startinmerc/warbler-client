12339a9e593c40f46018ee39de7c0ef39c3f2962
STM <startinmerc@gmail.com>
2020-03-23 15:56:46 +0000

    Add user actions

---
+++ src/store/actions/users.js
@@ -0,0 +1,29 @@
+import { apiCall } from "../../services/api";
+import { addError } from "./errors";
+import { LOAD_ALL_USERS } from "../actionTypes";
+
+export const loadAllUsers = users => ({
+	type: LOAD_ALL_USERS,
+	users
+});
+
+// export const loadOneUser = user => ({
+// 	type: LOAD_ONE_USER,
+// 	user
+// });
+
+export const fetchAllUsers = () => {
+	return dispatch => {
+		return apiCall("get", "/api/users")
+			.then(res => dispatch(loadAllUsers(res)))
+			.catch(err => dispatch(addError(err.message)));
+	};
+};
+
+// export const fetchOneUser = (user_id) => {
+// 	return dispatch => {
+// 		return apiCall("get", `/api/users/${user_id}`)
+// 			.then(res => dispatch(loadOneUser(res)))
+// 			.catch(err => dispatch(addError(err.message)));
+// 	};
+// };
