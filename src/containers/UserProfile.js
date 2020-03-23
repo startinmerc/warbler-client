7a27c58bd2d6f810c504695dd0672adafa0af1d6
STM <startinmerc@gmail.com>
2020-03-23 17:51:10 +0000

    Convert UserProfile to Class & add fetchOneUser

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -1,22 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
 import MessageList from "./MessageList";
+import { fetchOneUser } from "../store/actions/users";
 
-const UserProfile = props => {
-	const user = props.location.pathname.split("/")[2];
-	return (
-		<div className="row">
-			<div className="col-12 col-md-10">
-				<div className="text-center">
-					Showing messages from {user}<br/>
-					<small>Yes this is wrong.</small>
+class UserProfile extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			user: {}
+		}
+	}
+
+	async popUser(){
+		await this.props.fetchOneUser(this.props.location.pathname.split("/")[2]);
+		this.setState({user: this.props.foundUser});
+	}
+
+	componentDidMount(){
+		this.popUser();
+	}
+	
+	render(){
+		return (
+			<div className="row">
+				<div className="col-12 col-md-10">
+					<div className="text-center">
+						Showing messages from {this.state.user.username}
+					</div>
+					<MessageList fromUser="true" user={this.state.user._id} />
 				</div>
-				<MessageList fromUser="true" user={user} location={props.location}/>
+				<div className="col-12 col-md-2">
+					<div className="adspace"/>
+				</div>
 			</div>
-			<div className="col-12 col-md-2">
-				<div className="adspace"/>
-			</div>
-		</div>
-	)
+		)
+	}
 };
 
-export default UserProfile;
+function mapStateToProps(state){
+	return {
+		foundUser: state.users.foundUser
+	};
+};
+
+export default connect(mapStateToProps, { fetchOneUser })(UserProfile);
