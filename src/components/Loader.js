815afba984a397041dd4ab584f8300d7258caf9f
STM <startinmerc@gmail.com>
2020-03-24 15:12:30 +0000

    Missed a border

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = ({ isLoading, text }) => {
 	if(isLoading){
 		return (
-			<div className="loader bg-dark p-5 rounded">
+			<div className="loader bg-dark p-5 rounded white-border">
 				{text}
 				<span className="loader__dot">.</span>
 				<span className="loader__dot">.</span>
