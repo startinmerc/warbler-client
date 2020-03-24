d54635ae39a6b7b6fd569fa4c3b4f47085339f66
STM <startinmerc@gmail.com>
2020-03-24 15:12:18 +0000

    Style UserProfile header

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import MessageList from "./MessageList";
 import { Link } from "react-router-dom";
 import EditProfileModal from "../components/EditProfileModal";
+import Icon from "../images/Icon";
 import { fetchOneUser } from "../store/actions/users";
 
 class UserProfile extends React.Component {
@@ -37,18 +38,32 @@ class UserProfile extends React.Component {
 		return (
 			<div className="row">
 				<div className="col-12 col-md-10">
-					<div className="text-center">
-						<h2>@{this.state.user.username}</h2>
-						<p>{this.state.user.bio}</p>
-					{/*Show edit profile link if user is correct*/}
+					<div className="d-flex flex-wrap flex-md-nowrap justify-content-center justify-content-md-between rounded white-border p-3">
+						{/* profile image or backup icon */}
+						{this.state.user.profileImageUrl ? (
+							<img src={this.state.user.profileImageUrl}
+								alt={this.state.user.username}
+								className="message-item__image img-fluid"
+							/>
+							) : (
+								<Icon classes="message-item__image" size="100px" color="var(--white)"/>
+							)
+						}
+						<div className="text-center">
+							<h2>@{this.state.user.username}</h2>
+							<p>{this.state.user.bio}</p>
+						</div>
+						{/*Show edit profile link if user is correct*/}
 						{this.props.currentUser.user.id === this.state.user._id && 
-							<Link className="btn btn-outline-warning" to={`${this.props.location.pathname}/edit`}>button</Link>
+							<Link className="btn btn-outline-warning align-self-center edit-profile-button" 
+							 to={`${this.props.location.pathname}/edit`}
+							 >Edit Profile</Link>
 						}
 					</div>
 					<MessageList fromUser="true" user={this.state.user._id} />
 				</div>
 				<div className="col-12 col-md-2">
-					<div className="adspace"></div>
+					<div className="adspace white-border"></div>
 				</div>
 				{/*Mount EditProfileModal if required, passing props*/}
 				{this.state.edit && <EditProfileModal
