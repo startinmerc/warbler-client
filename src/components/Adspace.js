0f0b45de533c6b978de6c839dcf92eb84e7a8a04
STM <startinmerc@gmail.com>
2020-08-05 16:24:55 +0200

    Fix Adspace border, hide on mobile

--- src/components/Adspace.js
+++ src/components/Adspace.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Adspace = ()=>(
 	<div className="col-12 col-md-2 mt-3 mt-md-0 pl-0">
-		<div className="adspace white-border"></div>
+		<div className="adspace white-border border-left-0 d-none d-md-block"></div>
 	</div>
 );
 
