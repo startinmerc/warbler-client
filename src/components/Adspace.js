f0e85f2f707cf53028974b4a95c22e2816a9fd25
STM <startinmerc@gmail.com>
2020-08-05 16:24:11 +0200

    Remove padding from Adspace

--- src/components/Adspace.js
+++ src/components/Adspace.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Adspace = ()=>(
-	<div className="col-12 col-md-2 mt-3 mt-md-0">
+	<div className="col-12 col-md-2 mt-3 mt-md-0 pl-0">
 		<div className="adspace white-border"></div>
 	</div>
 );
