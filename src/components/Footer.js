1c3ddf43e52d035f3b8eea98ced6aa4efc7dc559
STM <startinmerc@gmail.com>
2020-08-05 16:33:03 +0200

    Decrease Footer padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-	<footer className="mb-0 py-4 d-flex justify-content-center">
+	<footer className="mb-0 py-2 d-flex justify-content-center">
 		<p className="mb-0 text-white">
 			A <a href="http://www.strangeindustries.co.uk">Strange Industries</a> site.
 		</p>
