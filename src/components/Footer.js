878e497c56aad3592a80ec3946720ea8ebedb423
STM <startinmerc@gmail.com>
2020-08-28 10:49:16 +0200

    External link to new tab in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
 	<footer className="mb-0 py-2 d-flex justify-content-center">
 		<p className="mb-0 text-white">
-			A <a href="http://www.strangeindustries.co.uk">Strange Industries</a> site.
+			A <a href="http://www.strangeindustries.co.uk" target="_blank" rel="noopener noreferrer">Strange Industries</a> site.
 		</p>
 	</footer>
 );
